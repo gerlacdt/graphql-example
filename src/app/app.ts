94698af08e30c170ee11438f70e979bf45e0fdac
Daniel Gerlach <daniel.gerlach1981@googlemail.com>
2020-02-19 21:00:03 +0100

    added mutations and middlware

--- src/app/app.ts
+++ src/app/app.ts
@@ -10,22 +10,62 @@ result: Int!
 }
 
 type RandomDie {
+ip: String
 numSides: Int!
 rollOnce: Int!
 roll(numRolls: Int!): [Int]
 }
 
+input MessageInput {
+content: String
+author: String
+}
+
+type Message {
+id: ID!
+content: String
+author: String
+}
+
 type Query {
 hello: String
 foo(id: String!): FooResponse
-
+getMessage(id: ID!): Message
 quoteOfTheDay: String
 random: Float!
 rollThreeDice: [Int]
 rollDice(numDice: Int!, numSides: Int): [Int]
 getDie(numSides: Int): RandomDie
 }
+
+type Mutation {
+createMessage(input: MessageInput): Message
+updateMessage(id: ID!, input: MessageInput): Message
+}
 `);
+
+const loggingMiddleware = (
+  req: express.Request,
+  _res: express.Response,
+  next: express.NextFunction,
+) => {
+  console.log("ip: %s", req.ip);
+  next();
+};
+
+class Message {
+  constructor(
+    public id: string,
+    public content: string,
+    public author: string,
+  ) {}
+}
+
+class MessageInput {
+  constructor(public content: string, public author: string) {}
+}
+
+const fakeDatabase: Record<string, { content: string; author: string }> = {};
 
 class RandomDie {
   constructor(public numSides: number) {}
@@ -72,10 +112,38 @@ const root = {
   getDie: ({ numSides = 6 }: { numSides?: number }) => {
     return new RandomDie(numSides);
   },
+
+  getMessage: ({ id }: { id: string }) => {
+    if (!fakeDatabase[id]) {
+      throw new Error("no message exists with id " + id);
+    }
+    const { content, author } = fakeDatabase[id];
+    return new Message(id, content, author);
+  },
+  createMessage: ({ input }: { input: MessageInput }) => {
+    // Create a random id for our "database".
+    var id = require("crypto")
+      .randomBytes(10)
+      .toString("hex");
+
+    fakeDatabase[id] = input;
+    const { content, author } = fakeDatabase[id];
+    return new Message(id, content, author);
+  },
+  updateMessage: ({ id, input }: { id: string; input: MessageInput }) => {
+    if (!fakeDatabase[id]) {
+      throw new Error("no message exists with id " + id);
+    }
+    // This replaces all old data, but some apps might want partial update.
+    fakeDatabase[id] = input;
+    const { content, author } = fakeDatabase[id];
+    return new Message(id, content, author);
+  },
 };
 
 export const app = express();
 
+app.use(loggingMiddleware);
 app.use(
   "/graphql",
   graphqlHTTP({
